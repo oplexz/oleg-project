7185d864b9d02a7c9ffc06737ce4f6310de4d650
Daniil Isakov <daniil.isakov@hotmail.com>
2024-05-27 03:50:44 +0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-    <div>
-        <nav class="navbar bg-base-100">
-            <div class="navbar-start">
-                <span>
+    <div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+            <div class="w-full navbar bg-base-300">
+                <div class="flex-none lg:hidden">
+                    <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            class="inline-block w-6 h-6 stroke-current"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            ></path>
+                        </svg>
+                    </label>
+                </div>
+                <div class="flex justify-between w-full">
                     <RouterLink to="/" class="btn btn-ghost text-xl">Красный Октябрь</RouterLink>
-                </span>
-            </div>
-            <div class="navbar-center hidden lg:flex">
-                <div class="menu menu-horizontal gap-2">
-                    <RouterLink to="/about" class="btn">О нас</RouterLink>
-                    <RouterLink to="/products" class="btn">Продукция</RouterLink>
-                    <RouterLink to="/store" class="btn">Магазин</RouterLink>
-                    <RouterLink to="/contacts" class="btn">Контакты</RouterLink>
+                    <div class="hidden lg:block">
+                        <div class="menu menu-horizontal gap-2">
+                            <RouterLink to="/about" class="btn">О нас</RouterLink>
+                            <RouterLink to="/products" class="btn">Продукция</RouterLink>
+                            <RouterLink to="/store" class="btn">Магазин</RouterLink>
+                            <RouterLink to="/contacts" class="btn">Контакты</RouterLink>
+                        </div>
+                    </div>
+                    <div class="menu menu-horizontal gap-2">
+                        <RouterLink to="/cart" class="btn">Тележка</RouterLink>
+                        <RouterLink to="/login" class="btn">Вход</RouterLink>
+                    </div>
                 </div>
             </div>
-            <div class="navbar-end">
-                <div class="menu menu-horizontal gap-2">
-                    <RouterLink to="/cart" class="btn">Тележка</RouterLink>
-                    <RouterLink to="/login" class="btn">Вход</RouterLink>
-                </div>
-            </div>
-        </nav>
+            <!-- Page content here -->
+            <RouterView />
+        </div>
+        <div class="drawer-side">
+            <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+            <ul class="menu p-4 w-80 min-h-full bg-base-200">
+                <!-- Sidebar content here -->
+                <li><RouterLink to="/about">О нас</RouterLink></li>
+                <li><RouterLink to="/products">Продукция</RouterLink></li>
+                <li><RouterLink to="/store">Магазин</RouterLink></li>
+                <li><RouterLink to="/contacts">Контакты</RouterLink></li>
+            </ul>
+        </div>
     </div>
-    <RouterView />
 </template>
 
 <script setup lang="ts"></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,74 @@
 <template>
-    <div class="flex">
-        <div class="w-64 p-4 bg-base-100 text-base-content">
-            <div class="text-2xl font-bold mb-4">Our Services</div>
-            <ul class="menu py-3 shadow-lg bg-base-100">
-                <li class="menu-title">
-                    <span>Delivery</span>
-                </li>
-                <li class="menu-title">
-                    <span>Master classes</span>
-                </li>
-                <li class="menu-title">
-                    <span>Custom cake orders</span>
-                </li>
-                <li class="menu-title">
-                    <span>Consultation</span>
-                </li>
-            </ul>
+    <div class="flex p-8 gap-2">
+        <div class="drawer w-64 p-4 bg-base-100 text-base-content flex flex-col justify-between">
+            <div>
+                <div class="text-2xl font-bold mb-4">Наши услуги</div>
+                <ul class="menu py-3 shadow-lg bg-base-100">
+                    <li class="menu-title">
+                        <span>Доставка</span>
+                    </li>
+                    <li class="menu-title">
+                        <span>Мастер классы</span>
+                    </li>
+                    <li class="menu-title">
+                        <span>Изготовление тортов на заказ</span>
+                    </li>
+                    <li class="menu-title">
+                        <span>Консультация</span>
+                    </li>
+                </ul>
+            </div>
+            <div>Адрес: 107410, г. Москва, ул. Малая Красносельская, 7 стр. 24</div>
         </div>
         <div class="flex gap-10">
             <div class="w-96 p-4 bg-base-100 text-base-content">
                 <div class="stats stats-vertical">
                     <div class="stat bg-base-100">
-                        <div class="stat-value">Более миллиарда</div>
+                        <div class="stat-value">Более 300</div>
                         <div class="stat-title">Наименований кондитерских изделий</div>
                     </div>
                     <div class="stat bg-base-100">
-                        <div class="stat-value">Дохуйлион</div>
+                        <div class="stat-value">70</div>
+                        <div class="stat-title">тыс. тонн готовой продукции в год</div>
+                    </div>
+                    <div class="stat bg-base-100">
+                        <div class="stat-value">35</div>
                         <div class="stat-title">Производственных линий</div>
                     </div>
                     <div class="stat bg-base-100">
-                        <div class="stat-value">1 мешок</div>
-                        <div class="stat-title">Готовой продукции в год</div>
-                    </div>
-                    <div class="stat bg-base-100">
-                        <div class="stat-value">1849 г. до н.э.</div>
+                        <div class="stat-value">1849</div>
                         <div class="stat-title">Год основания фабрики</div>
                     </div>
                 </div>
             </div>
-            <div class="card w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img :src="'museum-tour.webp'" alt="Museum" />
-                </figure>
-                <div class="card-body">
-                    <div class="card-title">Приглашаем на экскурсию по музею</div>
+            <div class="flex gap-4">
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img :src="'museum-tour.webp'" alt="Музей" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="card-title">Приглашаем на экскурсию по музею</div>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img :src="'candy-box.webp'" alt="Коробка конфет" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="card-title">Объединенные кондитеры: 20 лет вместе</div>
+                    </div>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img :src="'museum-tour.webp'" alt="Museum" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="card-title">Сеть фирменных магазинов Алёнка</div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
-
-const services = ref(["Delivery", "Master classes", "Custom cake orders", "Consultation"]);
-</script>
+<script setup lang="ts"></script>
