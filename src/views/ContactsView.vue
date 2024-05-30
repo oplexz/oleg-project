1bca925b40a6fe32cfc87c699a85c624c62e85d0
Daniil Isakov <daniil.isakov@hotmail.com>
2024-05-30 15:07:16 +0300

    Add ContactsView.vue

---
+++ src/views/ContactsView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="my-10 flex flex-col gap-4 text-center">
+        <div class="flex flex-col items-center justify-center gap-8">
+            <div class="font-bold text-5xl">Наш адрес</div>
+            <div class="flex items-center justify-center gap-10 text-2xl">
+                <div class="">107410, г. Москва, ул. Малая Красносельская, 7 стр. 24</div>
+                <div class="btn">Схема проезда</div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="flex flex-col items-center justify-center gap-8">
+            <div class="font-bold text-5xl">Приемная фабрики</div>
+            <div class="text-xl">
+                <div class="text-neutral">Телефон приемной</div>
+                <div class=""><a href="tel:+74959825601">+7 (495) 982-56-01</a></div>
+            </div>
+            <div class="text-xl">
+                <div class="text-neutral">Факс приемной</div>
+                <div class=""><a href="tel:+74959825602">+7 (495) 982-56-02</a></div>
+            </div>
+        </div>
+        <div class="divider"></div>
+        <div class="flex flex-col items-center justify-center gap-8">
+            <div class="font-bold text-5xl">Канцелярия фабрики</div>
+            <div class="text-xl">
+                <div class="text-neutral">Телефон канцелярии</div>
+                <div class=""><a href="tel:+74959825632">+7 (495) 982-56-32</a></div>
+            </div>
+            <div class="text-xl">
+                <div class="text-neutral">Факс канцелярии</div>
+                <div class=""><a href="tel:+74959825602">+7 (495) 982-56-02</a></div>
+            </div>
+            <div class="text-xl">
+                <div class="text-neutral">E-mail</div>
+                <div class=""><a href="mailto:ko@redoct.biz">ko@redoct.biz</a></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
