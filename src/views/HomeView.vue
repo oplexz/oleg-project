731b72c4cb87dd81a308c558b1601b57e2dbaef3
Daniil Isakov <daniil.isakov@hotmail.com>
2024-05-30 15:07:07 +0300

    Update HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,50 +20,52 @@
             </div>
             <div>Адрес: 107410, г. Москва, ул. Малая Красносельская, 7 стр. 24</div>
         </div>
-        <div class="flex gap-10">
-            <div class="w-96 p-4 bg-base-100 text-base-content">
-                <div class="stats stats-vertical">
-                    <div class="stat bg-base-100">
-                        <div class="stat-value">Более 300</div>
-                        <div class="stat-title">Наименований кондитерских изделий</div>
-                    </div>
-                    <div class="stat bg-base-100">
-                        <div class="stat-value">70</div>
-                        <div class="stat-title">тыс. тонн готовой продукции в год</div>
-                    </div>
-                    <div class="stat bg-base-100">
-                        <div class="stat-value">35</div>
-                        <div class="stat-title">Производственных линий</div>
-                    </div>
-                    <div class="stat bg-base-100">
-                        <div class="stat-value">1849</div>
-                        <div class="stat-title">Год основания фабрики</div>
-                    </div>
-                </div>
-            </div>
-            <div class="flex gap-4">
-                <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img :src="'museum-tour.webp'" alt="Музей" />
-                    </figure>
-                    <div class="card-body">
-                        <div class="card-title">Приглашаем на экскурсию по музею</div>
+        <div class="">
+            <div class="flex gap-10">
+                <div class="w-96 p-4 bg-base-100 text-base-content">
+                    <div class="stats stats-vertical">
+                        <div class="stat bg-base-100">
+                            <div class="stat-value">Более 300</div>
+                            <div class="stat-title">Наименований кондитерских изделий</div>
+                        </div>
+                        <div class="stat bg-base-100">
+                            <div class="stat-value">70</div>
+                            <div class="stat-title">тыс. тонн готовой продукции в год</div>
+                        </div>
+                        <div class="stat bg-base-100">
+                            <div class="stat-value">35</div>
+                            <div class="stat-title">Производственных линий</div>
+                        </div>
+                        <div class="stat bg-base-100">
+                            <div class="stat-value">1849</div>
+                            <div class="stat-title">Год основания фабрики</div>
+                        </div>
                     </div>
                 </div>
-                <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img :src="'candy-box.webp'" alt="Коробка конфет" />
-                    </figure>
-                    <div class="card-body">
-                        <div class="card-title">Объединенные кондитеры: 20 лет вместе</div>
+                <div class="flex gap-4">
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure>
+                            <img :src="'museum-tour.webp'" alt="Музей" />
+                        </figure>
+                        <div class="card-body">
+                            <div class="card-title">Приглашаем на экскурсию по музею</div>
+                        </div>
                     </div>
-                </div>
-                <div class="card w-96 bg-base-100 shadow-xl">
-                    <figure>
-                        <img :src="'museum-tour.webp'" alt="Museum" />
-                    </figure>
-                    <div class="card-body">
-                        <div class="card-title">Сеть фирменных магазинов Алёнка</div>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure>
+                            <img :src="'candy-box.webp'" alt="Коробка конфет" />
+                        </figure>
+                        <div class="card-body">
+                            <div class="card-title">Объединенные кондитеры: 20 лет вместе</div>
+                        </div>
+                    </div>
+                    <div class="card w-96 bg-base-100 shadow-xl">
+                        <figure>
+                            <img :src="'museum-tour.webp'" alt="Museum" />
+                        </figure>
+                        <div class="card-body">
+                            <div class="card-title">Сеть фирменных магазинов Алёнка</div>
+                        </div>
                     </div>
                 </div>
             </div>
